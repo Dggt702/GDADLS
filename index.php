40cae58110dfd896dcd6810511e66e5d69a64e9f
David Caicedo <caicedo4.20002@gmail.com>
2024-05-17 21:55:00 +0200

    Nuevo índice

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <main>
+        <div class="container">
+            <h1>Arbitraje</h1>
+        </div>
+    </main>
+</body>
+</html>
