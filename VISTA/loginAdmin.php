f1ebc896f905b0827009aade60980daddee436c5
David Caicedo <caicedo4.20002@gmail.com>
2024-05-17 23:17:35 +0200

    Corrección de errores

--- VISTA/loginAdmin.php
+++ VISTA/loginAdmin.php
@@ -1,7 +1,7 @@
-<form action="comprobarAdmin.php" method="POST" id="formAdmin" style="display: none">
+<form action="CONTROLADOR/acceder.php" method="POST" id="formAdmin" style="display: none">
     <div class="col-12">
-        <label for="nombreUsuarioAdmin" class="form-label">Nombre de usuario</label>
-        <input type="text" class="form-control" id="nombreUsuarioAdmin" name="nombreUsuarioAdmin">
+        <label for="nombreUsuario" class="form-label">Nombre de usuario</label>
+        <input type="text" class="form-control" id="nombreUsuario" name="nombreUsuario">
     </div>
     <div class="col-12">
         <label for="contraseniaAdmin" class="form-label">Contraseña</label>
