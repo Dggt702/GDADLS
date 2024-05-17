387da30ed0dd61c3a0e0a2bb1062721eb8b611b1
David Caicedo <caicedo4.20002@gmail.com>
2024-05-17 21:52:03 +0200

    Inserción de la conexión a la BBDD y código sql

---
+++ MODELO/BBDD.php
@@ -0,0 +1,19 @@
+<?php
+
+class BBDD {
+
+    public static function conectar(){
+       $server="localhost";
+       $dbname="Acreditaciones";
+       $user="root";
+       $password="";
+       
+        try{        
+            $connection = new PDO("mysql:host=$server;dbname=$dbname", $user, $password);
+            $connection->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+            return $connection;
+        } catch(PDOException $e){
+            die("ERROR: ".$e->getMessage());
+        }       
+    }
+}
