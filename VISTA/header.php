c1201ffe4e06239121195dc68eb9cefd50da5d69
David Caicedo <caicedo4.20002@gmail.com>
2024-05-17 22:08:32 +0200

    Primeras vistas

---
+++ VISTA/header.php
@@ -0,0 +1,7 @@
+<header class="bg-dark">
+    <div class="row text-light m-0">
+        <div class="col-12 text-center">
+            <h1>ARBITRAJE</h1>
+        </div>
+    </div>
+</header>
--- index.php
+++ index.php
@@ -1,15 +1,26 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    
     <title>Document</title>
 </head>
 <body>
+    <?php include_once "VISTA/header.php" ?>
     <main>
         <div class="container">
-            <h1>Arbitraje</h1>
+            <div class="row">
+                <form action="comprobarUsuario.php" method="POST">
+                    <div class="col-12">
+                        <label for="nombreUsuario" class="form-label">Nombre de usuario</label>
+                        <input type="text" class="form-control" id="nombreUsuario">
+                    </div>
+                </form>
+            </div>
         </div>
     </main>
+    <?php include_once "VISTA/header.php" ?>
 </body>
 </html>
