910bb69b2d232a2191de24318bcd5e824e2dad3f
DGGT702 <129065981+Dggt702@users.noreply.github.com>
2024-06-02 00:34:11 +0200

    Cambio 2-06-2024 00-34

--- index.php
+++ index.php
@@ -4,7 +4,6 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    
     <title>Document</title>
 </head>
 <body class="d-flex flex-column h-100">
@@ -13,10 +12,10 @@
         <div class="container">
             <div class="row">
                 <div class="col-6 d-flex justify-content-center">
-                    <button id="btnAdmin" class="btn">Administrador</button>
+                    <button id="btnAdmin" class="btn btn-success">Administrador</button>
                 </div>
                 <div class="col-6 d-flex justify-content-center">
-                    <button id="btnArbitro" class="btn">Árbitro</button>
+                    <button id="btnArbitro" class="btn btn-primary">Árbitro</button>
                 </div>
                 <div class="col-12">
                     <?php include_once "VISTA/loginAdmin.php" ?>
@@ -27,6 +26,7 @@
     </main>
     <?php include_once "VISTA/footer.php" ?>
 
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
     <script>
         document.addEventListener("DOMContentLoaded", function() {
             const btnAdmin = document.getElementById("btnAdmin");
